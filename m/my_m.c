1a2d817c4ab0ad6b6d6ce3d7fdf5f769fe2cd65d
wj <iamwjlee@gmail.com>
2014-02-06 17:49:52 +0900

    -

--- m/my_m.c
+++ m/my_m.c
@@ -24,7 +24,9 @@
 http://forum.falinux.com/zbxe/index.php?document_srl=406200&mid=device_driver
 http://www.freesoftwaremagazine.com/articles/drivers_linux
 http://lwn.net/Kernel/LDD3/
+http://xenostudy.tistory.com/432
 
+http://www.kandroid.org/board/board.php?board=linux&command=body&no=2
 
 */
 #include <linux/init.h>
@@ -43,21 +45,24 @@ static int   sz_data = 0;
 
 static int my_open(struct inode *inode, struct file *filp)
 {
-	printk("opened");
+	printk("[M] opened\n");
 	return 0;
 }
 static int my_release(struct inode *inode, struct file *filp)
 {
-	printk("released");
+	printk("[M] released\n");
 	return 0;
 }
 
 static ssize_t my_write( struct file *filp, const char *buf, size_t count, loff_t *f_pos )
 {
-   printk( "[VB] write to buffern");
+	
+   printk( "[M]write to buffern[%d]\n",count);
+
+
 
    if (BUFF_SIZE < count)  sz_data  = BUFF_SIZE;
-                           sz_data  = count;
+	else        sz_data  = count;
 
    strncpy( buffer, buf, sz_data);
    return count;
@@ -65,10 +70,20 @@ static ssize_t my_write( struct file *filp, const char *buf, size_t count, loff_
 
 static ssize_t my_read( struct file *filp, char *buf, size_t count, loff_t *f_pos )
 {
-  printk( "[VB] read from buffern" );
+  //printk( "Transfering data to user space" );
   copy_to_user( buf, buffer, sz_data);
-
+	
   return sz_data;
+	//
+	if(*f_pos==0)
+	{
+		*f_pos+=1;
+		return 1;
+	}
+	else
+		return 0;
+	//
+
 }
 
 static struct file_operations vd_fops = {
@@ -83,14 +98,14 @@ static int my_m_init(void)
 	register_chrdev( MAJOR_NUMBER, "virtual_buffer", &vd_fops );
   buffer = (char*) kmalloc( BUFF_SIZE, GFP_KERNEL );
   memset( buffer, 0, BUFF_SIZE);
-	printk(" 2014-02-05 my module test\n");
+	printk(KERN_ALERT " 2014-02-05 my module test\n");
 	return 0;
 }
 static void my_m_exit(void)
 {
 	  unregister_chrdev( MAJOR_NUMBER, "virtual_buffer" );
   kfree( buffer );
-	printk(" 2014-02-05 my module exit\n");
+	printk(KERN_ALERT " 2014-02-05 my module exit\n");
 }
 module_init(my_m_init);
 module_exit(my_m_exit);
